c15c18a10e6ac3f0868135f275ca2c303de33657
Lucas Neves <lucas.neves12@icloud.com>
2023-11-07 10:55:07 -0300

    Using ForEach in SwiftUI with an object.

--- ForEachSwiftUI/ForEachSwiftUI/ContentView.swift
+++ ForEachSwiftUI/ForEachSwiftUI/ContentView.swift
@@ -7,13 +7,52 @@
 
 import SwiftUI
 
+struct Person: Identifiable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
 struct ContentView: View {
+    
+    let list: [String] = ["Caio", "Felipe", "Lucas"]
+    
+    var listPerson: [Person] = [
+        Person(name: "Caio", age: 21),
+        Person(name: "Felipe", age: 33),
+        Person(name: "Lucas", age: 26),
+    ]
+    
     var body: some View {
+        
+        //MARK: ForEach simples
+        
+//        VStack {
+//            ForEach(0..<10) { value in
+//                Text("Valor -> \(value)")
+//            }
+//        }
+//        .padding()
+        
+        //MARK: ForEach com array simples
+        
+//        VStack {
+//            ForEach(list, id: \.self) { value in
+//                Text("Valor -> \(value)")
+//            }
+//        }
+//        .padding()
+        
+        //MARK: ForEach com array de objeto
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(listPerson) { person in
+                VStack(spacing: 5) {
+                    Text("Nome -> \(person.name)")
+                    Text("Idade -> \(person.age)")
+                }
+                
+            }
         }
         .padding()
     }
