c0ee0bb5397efeb3c4d2c098f5113d7eb3c8e6dd
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-07 10:26:40 -0300

    Add files via upload

---
+++ ForEachSwiftUI/ForEachSwiftUI/ForEachSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEachSwiftUIApp.swift
+//  ForEachSwiftUI
+//
+//  Created by Lucas Neves dos santos pompeu on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ForEachSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
